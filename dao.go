9de5620ff5979736e3ad83469cd18d761e674c5f
mgr-huawei <mgr9525@qq.com>
2019-09-07 11:44:20 +0800

    dao+

--- dao.go
+++ dao.go
@@ -7,7 +7,7 @@ import (
 
 type Dao struct {
 	db       *xorm.Engine
-	tempName string
+	TempName string
 
 	GetModel  func() interface{}
 	GetModels func() interface{}
@@ -31,11 +31,11 @@ func (c *Dao) getDb() *xorm.Engine {
 	return Db
 }
 func (c *Dao) SetTempName(s string) {
-	c.tempName = s
+	c.TempName = s
 }
 func (c *Dao) FindOne(pars *map[string]interface{}) interface{} {
 	m := c.GetModel()
-	has, err := c.getDb().SqlTemplateClient(c.tempName, pars).Get(m)
+	has, err := c.getDb().SqlTemplateClient(c.TempName, pars).Get(m)
 	if err != nil {
 		println(err.Error())
 		return nil
@@ -80,7 +80,7 @@ func (c *Dao) FindList(pars *map[string]interface{}) interface{} {
 		panic("not override GetModels")
 	}
 	m := c.GetModels()
-	err := c.getDb().SqlTemplateClient(c.tempName, pars).Find(m)
+	err := c.getDb().SqlTemplateClient(c.TempName, pars).Find(m)
 
 	if err != nil {
 		println(err.Error())
@@ -95,7 +95,7 @@ func (c *Dao) FindCount(pars *map[string]interface{}) int64 {
 	}
 	ret := int64(0)
 	(*pars)["getCount"] = 1
-	ok, err := c.getDb().SqlTemplateClient(c.tempName, pars).Get(&ret)
+	ok, err := c.getDb().SqlTemplateClient(c.TempName, pars).Get(&ret)
 	if err != nil {
 		println(err.Error())
 		return 0
@@ -132,7 +132,7 @@ func (c *Dao) FindPage(pars *map[string]interface{}, page int64, size interface{
 		}
 	}
 	start := (pageno - 1) * sizeno
-	err := c.getDb().SqlTemplateClient(c.tempName, pars).Limit(int(sizeno), int(start)).Find(m)
+	err := c.getDb().SqlTemplateClient(c.TempName, pars).Limit(int(sizeno), int(start)).Find(m)
 	if err != nil {
 		println(err.Error())
 		return nil
--- examples/go.mod
+++ examples/go.mod
@@ -1,6 +1,6 @@
 module examples
 
 require (
-	github.com/mgr9525/go-cloud v1.0.1
+	github.com/mgr9525/go-cloud v1.0.4-0.20190907034040-52078feae16b
 	gopkg.in/macaron.v1 v1.3.4
 )
