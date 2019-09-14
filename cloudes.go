1cd7ba880351f2d95511561d9f21d44cd186e323
LinskRuis.3.6GHz <mgr9525@qq.com>
2019-09-14 23:35:43 +0800

    json http

--- cloudes.go
+++ cloudes.go
@@ -1,6 +1,7 @@
 package gocloud
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -41,6 +42,41 @@ func (c *CloudExecs) execHttp(path string, data *url.Values) (int, []byte, error
 	}
 	return res.StatusCode, bts, nil
 }
+func (c *CloudExecs) execHttpJSON(path string, body interface{}) (int, []byte, error) {
+	host := c.Host
+	if len(host) <= 0 && len(c.Serv) > 0 && Consul != nil {
+		services, err := Consul.Agent().Services()
+		if err != nil {
+			return 0, nil, err
+		}
+		service := services[c.Serv]
+		if service == nil {
+			return 0, nil, errors.New("no service")
+		}
+		host = fmt.Sprintf("http://%s:%d", service.Address, service.Port)
+	}
+	if body == nil {
+		body = &map[string]interface{}{}
+	}
+
+	bts, err := json.Marshal(body)
+	if err != nil {
+		return 0, nil, err
+	}
+
+	req, err := http.NewRequest("P0ST", host+path, bytes.NewBuffer(bts))
+	if err != nil {
+		return 0, nil, err
+	}
+	req.Header.Set("Content-Type", "application/json;charset=utf-8")
+	res, err := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	byts, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return 0, nil, err
+	}
+	return res.StatusCode, byts, nil
+}
 
 func (c *CloudExecs) Exec(path string, pars *url.Values) (int, string, error) {
 	code, bts, err := c.execHttp(path, pars)
@@ -51,6 +87,26 @@ func (c *CloudExecs) Exec(path string, pars *url.Values) (int, string, error) {
 }
 func (c *CloudExecs) ExecObj(path string, pars *url.Values, ret interface{}) error {
 	_, bts, err := c.execHttp(path, pars)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(bts, ret)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *CloudExecs) ExecJSON(path string, pars interface{}) (int, string, error) {
+	code, bts, err := c.execHttpJSON(path, pars)
+	if err != nil {
+		return code, "", err
+	}
+	return code, string(bts), nil
+}
+func (c *CloudExecs) ExecObjJSON(path string, pars interface{}, ret interface{}) error {
+	_, bts, err := c.execHttpJSON(path, pars)
 	if err != nil {
 		return err
 	}
