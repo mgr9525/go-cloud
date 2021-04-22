bbb13cca4401d53b5e2e5f34dc130ec3d79faf1e
LinskRuis.32G <mgr9525@qq.com>
2021-04-22 10:57:16 +0800

    vcode

--- util.go
+++ util.go
@@ -2,8 +2,10 @@ package gocloud
 
 import (
 	"github.com/gin-gonic/gin"
+	"math/rand"
 	"net/http"
 	"strings"
+	"time"
 )
 
 func ClearXSS(src string) string {
@@ -33,4 +35,18 @@ func MidAccessAllowFun(c *gin.Context) {
 	}
 	// 处理请求
 	c.Next()
+}
+func GetRandStr(n int) (randStr string) {
+	chars := "ABCDEFGHIJKMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz23456789"
+	charsLen := len(chars)
+	if n > 10 {
+		n = 10
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < n; i++ {
+		randIndex := rand.Intn(charsLen)
+		randStr += chars[randIndex : randIndex+1]
+	}
+	return randStr
 }
