5c02e560193f9dc7764071c4ea3381a62d3a2b7e
LinsRuis.HW1 <mgr9525@qq.com>
2025-04-27 16:16:45 +0800

    +

--- dao.go
+++ dao.go
@@ -1,10 +1,12 @@
 package gocloud
 
 import (
+	"context"
+
 	"xorm.io/xorm"
 )
 
-//DBHelper db
+// DBHelper db
 type DBHelper struct {
 	db *xorm.Engine
 }
@@ -27,4 +29,16 @@ NewSession : get a new xorm.Session
 */
 func (c *DBHelper) NewSession() *xorm.Session {
 	return c.db.NewSession()
+}
+
+// NewSessions: get a new xorm.Session with auto close
+func (c *DBHelper) NewSessions(ctxs ...context.Context) *xorm.Session {
+	var ctx context.Context
+	if len(ctxs) > 0 {
+		ctx = ctxs[0]
+	}
+	if ctx == nil {
+		ctx = context.Background()
+	}
+	return c.db.Context(ctx)
 }
